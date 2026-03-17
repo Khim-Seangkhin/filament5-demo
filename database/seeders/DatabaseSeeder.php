45d92b6e66a6ae93e07ad9f2df8ec0398c3a6e8c
Khim Seangkhin <99263854+Khim-Seangkhin@users.noreply.github.com>
2026-03-17 13:44:24 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => 123
-        ]);
+        User::factory()->create();
     }
 }
